971720b15633fb444a4b698e68a4c62d7d924cb9
jliang-26 <jliang26@students.claremontmckenna.edu>
2025-10-02 11:05:56 -0700

    Add files via upload

---
+++ thesis1.R
@@ -0,0 +1,44 @@
+# Simulate 10,000 rankings for n candidates by assigning a probability to each candidate.  
+simulated_data = function(candidates, support){
+  n_sim = 10000
+  results = matrix(NA, nrow = n_sim, ncol = length(candidates))
+  set.seed(67)
+  for (i in 1:n_sim){
+    results[i, ] = sample(candidates, size = length(candidates), replace = FALSE, prob = support)
+  }
+  
+  results
+}
+
+# Compute paramters of the Dirichlet Posterior
+dirichlet_posterior = function(results, candidates) {
+  n_sequences = factorial(length(candidates))
+  # Count occurrences of each ranking. 
+  df = as.data.frame(results)
+  strings = apply(df, 1, paste, collapse = "-")
+  sequences = unique(strings)
+  counts = table(factor(strings, levels = sequences))
+
+  # Compute posterior
+  alpha_prior = rep(1, n_sequences)
+  alpha_post = alpha_prior + as.numeric(counts)
+  
+  names(alpha_post) = sequences
+  
+  alpha_post
+}
+
+# Set candidates and percentage of support. 
+candidates = c("A", "B", "C", "D")
+
+support = c(.4, .3, .2, .1)
+
+# Simulate rankings and compute a Dirichlet Posterior.
+sample_rankings = simulated_data(candidates, support)
+
+posterior = dirichlet_posterior(sample_rankings, candidates)
+
+library(MCMCpack)
+sample_polls <- rdirichlet(10000, posterior)
+colnames(sample_polls) <- names(posterior)
+
