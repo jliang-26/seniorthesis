386e42ba2ed94aa2ef26e28b60cefa5886e2f419
jliang-26 <jliang26@students.claremontmckenna.edu>
2025-10-30 21:10:26 -0700

    Add files via upload

--- thesis(10.30).R
+++ thesis(10.30).R
@@ -4,21 +4,21 @@ set.seed(67)
 # Function 1: Simulate a poll of "sample_size" # of rankings for "candidates" by assigning a 
 # probability of "support" to each candidate.  
 simulated_data = function(candidates, support, sample_size){
-  results = matrix(NA, nrow = (sample_size), ncol = length(candidates))
+  sample_rankings = matrix(NA, nrow = (sample_size), ncol = length(candidates))
   for (i in 1:sample_size){
-    results[i, ] = sample(candidates, size = length(candidates), replace = FALSE, prob = support)
+    sample_rankings[i, ] = sample(candidates, size = length(candidates), replace = FALSE, prob = support)
   }
   
-  results
+  sample_rankings
 }
 
-# Function 2: Compute parameters of the Dirichlet Posterior using "results" from function 1 poll
-dirichlet_posterior = function(results, candidates) {
+# Function 2: Compute parameters of the Dirichlet Posterior using "sample_rankings"
+dirichlet_posterior = function(sample_rankings, candidates) {
   n = factorial(length(candidates))
   # Count occurrences of each ranking as a table of unique rankings.
-  df = as.data.frame(results)
+  df = as.data.frame(sample_rankings)
   strings = apply(df, 1, paste, collapse = "-")
-  sequences = unique(strings)
+  sequences = sort(unique(strings))
   counts = table(factor(strings, levels = sequences))
   
   # Compute posterior
@@ -30,18 +30,30 @@ dirichlet_posterior = function(results, candidates) {
   alpha_post
 }
 
+
+## TEST CODE
+#candidates = c(1, 2, 3, 4)
+#support = c(.31, .28, .25, .16)
+#sample_size = 500
+#sample_rankings = simulated_data(candidates, support, sample_size)
+
+#posterior = dirichlet_posterior(sample_rankings, candidates)
+#sample_probs = rdirichlet(sample_size, posterior)
+#colnames(sample_probs) = names(posterior)
+## TEST CODE
+
 # Function 3: Simulates RCV. Function takes set of unique rankings and probabilities from 
-# "sample_polls" and number of candidates from "candidates"
-run_rcv = function(sample_polls, candidates) {
+# "sample_probs" and number of candidates from "candidates"
+run_rcv = function(sample_probs, candidates) {
   c = length(candidates)
-  p = ncol(sample_polls)
-  n = nrow(sample_polls)
+  p = ncol(sample_probs)
+  n = nrow(sample_probs)
   winner = rep(0, n)
   
   #Create the RCV matrix. Row 1 is sampled probabilities, row 2 is a
   # tool to eliminate candidates, and subsequent rows are the rankings.
   RCV = matrix(0, nrow = c + 2, ncol = factorial(c))
-  orderings = colnames(sample_polls)
+  orderings = colnames(sample_probs)
   for (i in 1:p) {
     num_order = as.numeric(strsplit(orderings[i],"-")[[1]])
     RCV[3:(c+2), i] = num_order
@@ -53,7 +65,7 @@ run_rcv = function(sample_polls, candidates) {
     add = rep(0, p)
     
     #Bring probabilities into RCV calculator
-    RCV[1, ] = sample_polls[r, ]
+    RCV[1, ] = sample_probs[r, ]
     
     #Tally round 1
     for (i in 1:p) {
@@ -61,7 +73,7 @@ run_rcv = function(sample_polls, candidates) {
       tally[j] = tally[j] + RCV[1,i]
     }
     
-    #While loop to do RCV canddiate elimination.
+    #While loop to do RCV candidate elimination.
     while (all(tally < 0.5)) {
       m = which.min(tally)
       for (i in 1:p) {
@@ -89,11 +101,11 @@ bayes_rcv_sim = function(candidates, support, sample_size) {
   posterior = dirichlet_posterior(sample_rankings, candidates)
   
   # Sample 500 probability distributions using posterior
-  sample_polls = rdirichlet(sample_size, posterior)
-  colnames(sample_polls) = names(posterior)
+  sample_probs = rdirichlet(sample_size, posterior)
+  colnames(sample_probs) = names(posterior)
   
   # Run RCV on probability distributions using function 3.
-  winners = run_rcv(sample_polls, candidates)
+  winners = run_rcv(sample_probs, candidates)
   
   #Report winners by percent and count.
   counts <- table(winners)
